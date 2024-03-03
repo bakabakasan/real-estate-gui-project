7623b100ed286abf4ce677c45b5a95ca390ba8c6
Nastassia Kazlouskaya <nastya95kv16@gmail.com>
2024-03-03 20:21:51 +0300

    modified:   real-estate-website-main/app.py new file:   real-estate-website-main/run.py

--- real-estate-website-main/app.py
+++ real-estate-website-main/app.py
@@ -15,6 +15,3 @@ def hello_dreamhouse():
 def list_estate():
   estate = load_estate_from_db(engine)
   return jsonify(estate=estate) 
-
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True) 
