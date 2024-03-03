7e8f242c114ba429500fb4138d88091c6c308e1a
Nastassia Kazlouskaya <nastya95kv16@gmail.com>
2024-03-03 14:33:59 +0300

    Editing security of database server data

--- real-estate-website-main/app.py
+++ real-estate-website-main/app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, jsonify 
-from database import load_estate_from_db
+from database import load_estate_from_db, create_db_engine
 
 app = Flask(__name__)
 
+config_filename = 'config.json'
+engine = create_db_engine(config_filename)
+
 @app.route("/") 
 def hello_dreamhouse(): 
-  estate = load_estate_from_db()
+  estate = load_estate_from_db(engine)
   return render_template('home.html', estate=estate) 
 
 @app.route("/api/estate")
 def list_estate():
-  estate = load_estate_from_db()
+  estate = load_estate_from_db(engine)
   return jsonify(estate=estate) 
 
 if __name__ == "__main__":
--- real-estate-website-main/database.py
+++ real-estate-website-main/database.py
@@ -1,16 +1,30 @@
+import json
 from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql+pymysql://root:damnedKoraline1@localhost/realestate?charset=utf8mb4")
+def read_config(filename):
+    with open(filename, 'r') as file:
+        config_data = json.load(file)
+    db_username = config_data['DB_USERNAME']
+    db_password = config_data['DB_PASSWORD']
+    db_host = config_data['DB_HOST']
+    db_name = config_data['DB_NAME']
+    return db_username, db_password, db_host, db_name
 
-def load_estate_from_db(): 
+def create_db_engine(config_filename):
+    db_username, db_password, db_host, db_name = read_config(config_filename)
+    db_connection_string = f"mysql+pymysql://{db_username}:{db_password}@{db_host}/{db_name}?charset=utf8mb4"
+    engine = create_engine(db_connection_string)
+    print("Connected to the database.")
+    return engine
+
+def load_estate_from_db(engine): 
     with engine.connect() as conn:
         result = conn.execute(text("select * from estate"))
-        estate = []
-        for row in result.fetchall():
-            estate.append(row)
+        estate = result.fetchall()
+    print("Query executed successfully.")
     return estate
 
-
-
-
-    
+# Usage
+config_filename = 'config.json'
+engine = create_db_engine(config_filename)
+estate = load_estate_from_db(engine)
