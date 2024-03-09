0cdae560cd856dce4db48028540712711fc0d041
Nastassia Kazlouskaya <nastya95kv16@gmail.com>
2024-03-09 19:36:56 +0300

    modified:   main/requirements.txt new file:   main/tempCodeRunnerFile.py

---
+++ main/tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, jsonify 
+from database import load_estate_from_db, create_db_engine
+
+app = Flask(__name__)
+
+engine = create_db_engine()
+
+@app.route("/") 
+def hello_dreamhouse(): 
+  estate = load_estate_from_db(engine)
+  return render_template('home.html', estate=estate) 
+
+@app.route("/api/estate")
+def list_estate():
+  estate = load_estate_from_db(engine)
+  return jsonify(estate=estate) 
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True) 
