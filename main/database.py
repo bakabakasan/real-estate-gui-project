4b24811011c81216f4ef92aeab3c9e772abe15cd
Nastassia Kazlouskaya <nastya95kv16@gmail.com>
2024-03-10 19:41:27 +0300

    modified:   main/database.py

--- main/database.py
+++ main/database.py
@@ -5,24 +5,31 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def read_config():
-    db_url = os.environ.get('DB_URL')
+    db_username = os.environ.get('DB_USERNAME')
+    db_password = os.environ.get('DB_PASSWORD')
+    db_host = os.environ.get('DB_HOST')
+    db_name = os.environ.get('DB_NAME')
 
-    print("DB_URL:", db_url)
-
-    return db_url
+    return db_username, db_password, db_host, db_name
 
 def create_db_engine():
-    db_url = read_config()
-    engine = create_engine(db_url)
+    db_username, db_password, db_host, db_name = read_config()
+    db_connection_string = f"postgresql://{db_username}:{db_password}@{db_host}/{db_name}"
+    engine = create_engine(db_connection_string)
     print("Connected to the database.")
     return engine
 
 def load_estate_from_db(engine): 
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM estate"))
-        estate = result.fetchall()
-    print("Query executed successfully.")
-    return estate
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text("SELECT * FROM estate"))
+            estate = result.fetchall()
+        print("Query executed successfully.")
+        return estate
+    except UnicodeDecodeError as e:
+        print("Error decoding data:", e)
+        # Handle the error gracefully, e.g., log it or skip the problematic data.
+        return None
 
 # Usage
 engine = create_db_engine()
