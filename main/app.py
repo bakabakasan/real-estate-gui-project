e14abbe34da2bdc6c1a4ae1f84fc0fa7c05f4b33
Nastassia Kazlouskaya <nastya95kv16@gmail.com>
2024-03-12 21:58:33 +0300

    modified:   main/app.py modified:   main/database.py

--- main/app.py
+++ main/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify, abort 
-from database import load_estate_from_db, create_db_engine, load_estates_from_db
+from flask import Flask, render_template, jsonify
+from database import load_estate_from_db, create_db_engine, load_estateitem_from_db
 
 app = Flask(__name__)
 
@@ -15,10 +15,13 @@ def list_estate():
   estates = load_estate_from_db(engine)
   return jsonify(estate=estates) 
 
-@app.route("/estates/<id>")
-def show_estates(id):
-  estates = load_estates_from_db(id)
-  return jsonify(estates)
+@app.route("/estateitem/<id>")
+def show_estate(id):
+  estate_item = load_estateitem_from_db(id)
+  if estate_item is None:  # Added a check for None to handle the case when no item is found
+        return jsonify(error="No estate item found for the given ID"), 404
+  else:
+        return jsonify(estate_item)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True) 
