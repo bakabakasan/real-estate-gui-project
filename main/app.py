cc4cb6450adae864afc8194aa2850f0068e17ffe
Nastassia Kazlouskaya <nastya95kv16@gmail.com>
2024-03-13 22:35:46 +0300

    modified:   main/app.py modified:   main/templates/aboutus.html modified:   main/templates/contacts.html deleted:    main/templates/contactus.html new file:   main/templates/contactusform.html modified:   main/templates/estatepage.html modified:   main/templates/footer.html modified:   main/templates/home.html new file:   main/templates/sent_message.html

--- main/app.py
+++ main/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_estate_from_db, create_db_engine, load_estateitem_from_db
 
 app = Flask(__name__)
@@ -8,20 +8,30 @@ engine = create_db_engine()
 @app.route("/") 
 def hello_dreamhouse(): 
   estate = load_estate_from_db(engine)
-  return render_template('home.html', estate=estate) 
+  return render_template('home.html', 
+                         estate=estate) 
 
 @app.route("/api/estate")
 def list_estate():
   estates = load_estate_from_db(engine)
   return jsonify(estate=estates) 
 
-@app.route("/estateitem/<id>")
+@app.route("/estate-item/<id>")
 def show_estate(id):
   estate_item = load_estateitem_from_db(id)
   if not estate_item:
     return "Not Found", 404
   
-  return render_template('estatepage.html', estate=estate_item)
+  return render_template('estatepage.html', 
+                         estate=estate_item)
+  
+@app.route("/sent-message", methods=['post'])
+def fill_in_the_form():
+  data = request.form
+  #store this in DB
+  #display an aknowlegment
+  return render_template('sent_message.html',
+                         message=data) 
 
 @app.route("/contacts") 
 def contact_details(): 
